0d4e0f71e6bb76a600d4395966bdfa93f23c3e03
Miralius <f-miralius@yandex.ru>
2026-03-02 17:28:24 +0000

    Переместил структуру `AesCipherParams` в cpp-файл

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,8 +1,19 @@
 #include "crypto_guard_ctx.h"
 
-#include <memory>
+#include <array>
+#include <openssl/evp.h>
 
 namespace CryptoGuard {
+
+struct AesCipherParams {
+    static const size_t KEY_SIZE = 32;             // AES-256 key size
+    static const size_t IV_SIZE = 16;              // AES block size (IV length)
+    const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
+
+    int encrypt;                              // 1 for encryption, 0 for decryption
+    std::array<unsigned char, KEY_SIZE> key;  // Encryption key
+    std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
+};
 
 class CryptoGuardCtx::PImpl {
 public:
--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,6 @@
 #include <stdexcept>
 #include <string>
 
-struct AesCipherParams {
-    static const size_t KEY_SIZE = 32;             // AES-256 key size
-    static const size_t IV_SIZE = 16;              // AES block size (IV length)
-    const EVP_CIPHER *cipher = EVP_aes_256_cbc();  // Cipher algorithm
-
-    int encrypt;                              // 1 for encryption, 0 for decryption
-    std::array<unsigned char, KEY_SIZE> key;  // Encryption key
-    std::array<unsigned char, IV_SIZE> iv;    // Initialization vector
-};
-
 AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
     AesCipherParams params;
     constexpr std::array<unsigned char, 8> salt = {'1', '2', '3', '4', '5', '6', '7', '8'};
