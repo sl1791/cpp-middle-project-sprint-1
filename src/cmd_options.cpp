d7cf0ffa09e537a0b61de345a6bc3b03d5ec2872
Miralius <f-miralius@yandex.ru>
2026-03-02 13:43:48 +0000

    Реализация `ProgramOptions` конструктора для поддержки новых команд: `help`, `command`, `input`, `output` и `password`.

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -2,7 +2,15 @@
 
 namespace CryptoGuard {
 
-ProgramOptions::ProgramOptions() : desc_("Allowed options") {}
+ProgramOptions::ProgramOptions() : desc_("Allowed options") {
+    desc_.add_options()
+        ("help", "показать все поддерживаемые команды")
+        ("command", boost::program_options::value<std::string>(), "команда encrypt, decrypt или checksum")
+        ("input", boost::program_options::value<std::string>(), "путь до входного файла")
+        ("output", boost::program_options::value<std::string>(), "путь до файла, где будет сохранен результат")
+        ("password", boost::program_options::value<std::string>(), "пароль для шифрования и дешифрова")
+    ;
+}
 
 ProgramOptions::~ProgramOptions() = default;
 
