8b697abe72b35b416067f06217cbf0a789259c60
Miralius <f-miralius@yandex.ru>
2026-03-02 16:59:32 +0000

    Определения методов `CryptoGuardCtx` перенесены в cpp.

--- include/crypto_guard_ctx.h
+++ include/crypto_guard_ctx.h
@@ -6,19 +6,19 @@ namespace CryptoGuard {
 
 class CryptoGuardCtx {
 public:
-    CryptoGuardCtx() {}
-    ~CryptoGuardCtx() {}
+    CryptoGuardCtx();
+    ~CryptoGuardCtx();
 
     CryptoGuardCtx(const CryptoGuardCtx &) = delete;
     CryptoGuardCtx &operator=(const CryptoGuardCtx &) = delete;
 
-    CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
-    CryptoGuardCtx &operator=(CryptoGuardCtx &&) noexcept = default;
+    CryptoGuardCtx(CryptoGuardCtx &&) noexcept;
+    CryptoGuardCtx &operator=(CryptoGuardCtx &&) noexcept;
 
     // API
-    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    std::string CalculateChecksum(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
+    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    std::string CalculateChecksum(std::iostream &inStream);
 
 private:
     class Impl;
--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,17 @@
+#include "crypto_guard_ctx.h"
+
+#include <memory>
 
 namespace CryptoGuard {
+
+CryptoGuardCtx::CryptoGuardCtx() : pImpl_(nullptr) {}
+CryptoGuardCtx::~CryptoGuardCtx() = default;
+CryptoGuardCtx::CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
+CryptoGuardCtx &CryptoGuardCtx::operator=(CryptoGuardCtx &&) noexcept = default;
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+
+std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
 
 }  // namespace CryptoGuard
