3ce81f8527ee4b5e5287b57f53e7b72ce4d161fd
Sergei Larin <user@DESKTOP-DNJ6ULK.localdomain>
2026-05-17 15:12:52 +0300

    Заготовка для CryptoGuardCtx::Impl

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,50 @@
+#include "crypto_guard_ctx.h"
 
 namespace CryptoGuard {
+
+class CryptoGuardCtx::Impl
+{
+public:
+    ~Impl() = default;
+    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    std::string CalculateChecksum(std::iostream &inStream);
+};
+
+void CryptoGuardCtx::Impl::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
+{
+
+}
+
+void CryptoGuardCtx::Impl::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password)
+{
+
+}
+
+std::string CryptoGuardCtx::Impl::CalculateChecksum(std::iostream &inStream)
+{
+    return "NOT_IMPLEMENTED"; 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream, 
+    std::iostream &outStream, std::string_view password)
+{
+    pImpl_->EncryptFile(inStream, outStream, password);
+}
+
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream, 
+    std::iostream &outStream, std::string_view password)
+{
+    pImpl_->DecryptFile(inStream, outStream, password);
+}
+
+std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream) 
+{ 
+    return pImpl_->CalculateChecksum(inStream);
+}
+
+CryptoGuardCtx::CryptoGuardCtx() = default;
+CryptoGuardCtx::~CryptoGuardCtx() = default;
 
 }  // namespace CryptoGuard
